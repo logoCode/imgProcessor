0e580138d76dbfb8ef8ec09f4925029d6457ec8a
Max Obermeier <maxo2000@outlook.de>
2017-02-11 17:02:06 +0100

    added license

--- Main.go
+++ Main.go
@@ -12,10 +12,10 @@ import(
 
 func main(){
     test()
-    fmt.Println("---------------------------------------------------------------")
-    fmt.Println("------------------ Welcome to ImageProcessor ------------------")
-    fmt.Println("------------------   (C)2017 Max Obermeier   ------------------")
-    fmt.Println("---------------------------------------------------------------")
+    fmt.Println("-------------------------------------------------------------------------")
+    fmt.Println("----------------------- Welcome to ImageProcessor -----------------------")
+    fmt.Println("-----------------------   (C)2017 Max Obermeier   -----------------------")
+    fmt.Println("-------------------------------------------------------------------------")
     var filename, identifier, separator string
     var accuracy int
     filename = "output.txt"
@@ -25,9 +25,11 @@ func main(){
     for {
         fmt.Println()
         fmt.Println("Enter help to get a list of options or type in any other command.")
-        input := scanner.GetS("==","help","colors","settings","process","exit")
+        input := scanner.GetS("==","help","license","colors","settings","process","exit")
         if input == "help" {
             help()
+        }else if input == "license" {
+            license()
         }else if input == "exit" {
             os.Exit(0)
         }else if input == "colors"{
@@ -41,6 +43,18 @@ func main(){
 
 }
 
+func license(){
+    fmt.Println("MIT License")
+    fmt.Println("Copyright (c) 2017 Max Obermeier")
+    fmt.Println("")
+    fmt.Println(`Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"`)
+    fmt.Println("")
+    fmt.Println("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.")
+    fmt.Println("")
+    fmt.Println(`THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`)
+    fmt.Println("")
+}
+
 func listColors(){
     fmt.Println("The third parameter of each highlighted line is portraied as a color.")
     fmt.Println("Here is a list of the colors, with its according value.")
@@ -52,13 +66,14 @@ func listColors(){
     fmt.Println("  - 5 \t \t=> purple")
     fmt.Println("  - 6 \t \t=> yellow")
     fmt.Println("  - 7 \t \t=> black")
-    fmt.Println("  - > 7 \t \t=> white")
+    fmt.Println("  - > 7 \t=> white")
 
 }
 
 func help(){
     fmt.Println("List of options:")
     fmt.Println("  - help \t \t=> Show list of options")
+    fmt.Println("  - license \t \t=> Show license")
     fmt.Println("  - settings \t \t=> Set processing parameters")
     fmt.Println("  - process \t \t=> Start image creating process")
     fmt.Println("  - colors \t \t=> Show list of colors")
